97965225ca2ba8ab76a0f579788eaa9b8b9019b2
Jörg Hoh <joerg@joerghoh.de>
2015-07-09 12:33:50 +0200

    Version differences are colored, so you can spot them easier.

--- scripts/bundle-comparison/generate_chart.pl
+++ scripts/bundle-comparison/generate_chart.pl
@@ -35,12 +35,27 @@ print "  </tr>\n";
 #print table rows
 foreach my $r (sort keys ($bundles)) {
   print "<tr><td>$r</td>";
+  my $tmp_version = "";
+  my $first = 1;
   foreach my $v (@sorted_versions) {
     if (exists $bundles->{$r}->{$v}) {
-      print "<td>" . $bundles->{$r}->{$v} . "</td>";
+      my $versionString = $bundles->{$r}->{$v};
+      if ($versionString eq $tmp_version) {
+        print "<td>" . $versionString .  "</td>";
+      } else {
+        if ($first == 1) {
+          print "<td>" . $versionString .  "</td>";
+	  $tmp_version = $versionString;
+          $first = 0;
+        } else {
+          print "<td style='color:red'>" . $versionString . "</td>";     
+	  $tmp_version = $versionString;
+        }
+      }
     } else {
       print "<td>-</td>";
     }
+
   }
   print "</tr>\n";
 }
